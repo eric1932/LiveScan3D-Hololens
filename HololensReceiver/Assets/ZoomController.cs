55c9410e08f02250bd54a36faf5bd96d0fdacf61
Eric <eric1932777@gmail.com>
2020-11-28 19:50:04 -0600

    Add ZoomController (but not used)

---
+++ HololensReceiver/Assets/ZoomController.cs
@@ -0,0 +1,57 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CubeInteractive : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
+
+using NRKernal;
+using UnityEngine;
+
+public class ZoomController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    void Update()
+    {
+        //get controller rotation, and set the value to the cube transform
+        transform.rotation = NRInput.GetRotation();
+
+        Vector2 vec = NRInput.GetDeltaTouch();
+        //Debug.Log(vec.x);
+        if (Mathf.Abs(vec.x) >= Mathf.Abs(vec.y))
+        {
+            // TODO Implement
+        }
+        else
+        {
+            if (vec.y >= 0)
+            {
+                // swipe down => zoom out
+                //transform.localScale *= 0.975f;
+                transform.localScale -= new Vector3(0.075f, 0.075f, 0.075f);
+            }
+            else
+            {
+                // swipe up => zoom in
+                //transform.localScale *= 1.025f;
+                transform.localScale += new Vector3(0.075f, 0.075f, 0.075f);
+            }
+        }
+    }
+}
